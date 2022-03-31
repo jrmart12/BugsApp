ee9ec3ad3ee654a4098fe7a8072d5e5885382540
Jrmart12 <jr.martinez@codex.academy>
2022-03-31 16:46:42 -0600

    combining reducers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import configureAppStore from "./store/configureStore";
 import { bugAdded, bugRemoved, bugResolved } from "./store/bugs";
-
+import { projectAdded } from "./store/projects";
 const store = configureAppStore();
 
 const unsubscribe = store.subscribe(() => {
   console.log("store changed", store.getState());
 });
 
+store.dispatch(projectAdded({ name: "project 1" }));
 store.dispatch(bugAdded({ description: "Bug1" }));
 store.dispatch(bugAdded({ description: "Bug2" }));
 store.dispatch(bugAdded({ description: "Bug2" }));
